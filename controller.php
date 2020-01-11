61e96707c450c7c13c6cea8d96c436640fbeb913
satara <louis.gehrisch@gmx.de>
2020-01-11 22:20:22 +0100

    All be better

--- controller.php
+++ controller.php
@@ -1,57 +1,183 @@
 <?php
 /*
- * Configuration
+ * $ php controller.php [testfunction] [repeats] [outputfile.csv]
+ * testfunction:    führt eine bestimmte Funktion aus
+ * repeats:         Wiederholungen
+ * outputfile.csv   Angabe des Outputfiles
  */
-$firstElementSize = 500;
-$lastElementSize = 5000;
-$step = 500;
+define("PERF_DIR", "perf/");
 /*
  * Erwartet eine Ausgabe der Befehle in Form von "\d+(\.\d*)?\[micros\]"
+ * $0 gibt das Position des Files an
+ * $1 gibt die Position der Argumente an
  */
-$commands = [
-    "php" => "php perfphp.php",
-    "c" => "gcc perfc.c -o c && ./c",
-    "cpp" => "g++ perfcpp.cpp -o cpp && ./cpp",
-    "js" => "node perfjs.js",
-    "python" => "python perfpy.py",
-    "java" => "java perfjava.java"
+$languages = [
+    "PHP" => [
+        "name" => "PHP",
+        "exec" => "php $0 $1",
+        "extension" => "php"
+    ],
+    "C" => [
+        "name" => "C",
+        "exec" => "gcc $0 -o c && ./c $1",
+        "extension" => "c"
+    ],
+    "C++" => [
+        "name" => "C++",
+        "exec" => "g++ $0 -o cpp && ./cpp $1",
+        "extension" => "cpp"
+    ],
+    "JS" => [
+        "name" => "JavaScript",
+        "exec" => "node $0 $1",
+        "extension" => "js"
+    ],
+    "Python" => [
+        "name" => "Python",
+        "exec" => "python $0 $1",
+        "extension" => "py"
+    ],
+    "Java" => [
+        "name" => "Java",
+        "exec" => "java $0 $1",
+        "extension" => "java"
+    ]
 ];
 /*
- * Command
- * php controllwe.php [repeats] [outputfile.csv]
- * Wiederholungen $repeats
- * Ausgabefile $outputfile
- * Gibt an wie oft der Test wiederholt wird
+ * Configuration
+ * Name must be equals filename
  */
-$repeats = $argv[1];
-$outputfile = $argv[2] ?? "result.csv";
+$testConfs = [
+    "bubble" => [
+        "firstElementSize" => 500,
+        "lastElementSize" => 5000,
+        "step" => 500
+    ]
+];
 
 
-//Controlling
-$results = [];
-$repeats = $repeats > 0 ? $repeats : 1;
-for ($count = $firstElementSize; $count <= $lastElementSize; $count = $count + $step) {
-    $result = ["elements" => $count];
-    foreach ($commands as $lang => $command) {
-        for ($s = 0; $s < $repeats; $s++) {
-            $return = exec($command . ' ' . $count);
-            echo $lang . "\t" . $count . "\t" . $return . PHP_EOL;
-            preg_match('/^(.*?)\[micros\]$/', $return, $match);
-            //Durchschnitt berechnen
-            $result[$lang] = (($result[$lang] ?? 0) * $s + intval($match[1])) / ($s + 1);
+$testfunction = $argv[1] ?? "";
+$controller = new TestController($languages, $testConfs, $argv[2] ?? 1);
+
+$tests = empty($testfunction) ? $controller->getTests() : [$testfunction];
+foreach ($tests as $test) {
+    $outputFile = $argv[3] ?? $test . "-result.csv";
+    saveResultAsCSV($controller->test($test), $outputFile);
+}
+unlink("c");
+unlink("cpp");
+
+class TestController
+{
+    protected $languages;
+    protected $confs;
+    protected $repeats;
+
+    public function __construct(array $languages, array $confs, int $repeats)
+    {
+        $this->languages = $languages;
+        $this->confs = $confs;
+        $this->repeats = $repeats;
+        $this->init();
+    }
+
+    protected function init()
+    {
+        foreach ($this->languages as $lang => &$langConf) {
+            $langConf["extension"] = $langConf["extension"] ?? $langConf["extension"] ?? $lang;
+            $files = glob(PERF_DIR . DIRECTORY_SEPARATOR . "*" . DIRECTORY_SEPARATOR . "*" . $langConf["extension"]);
+            foreach ($files as $file) {
+                $langConf["files"][pathinfo($file)["filename"]] = $file;
+            }
         }
-        $result[$lang] = intval($result[$lang]);
-        if($repeats>1){
-            echo "AVG:\t" . $result[$lang]."[micros]" . PHP_EOL;
+    }
+
+    public function getTests()
+    {
+        $tests = [];
+        foreach ($this->languages as $langConf) {
+            foreach ($langConf["files"] as $file) {
+                $tests[] = pathinfo($file)["filename"] ?? null;
+            }
         }
+        return array_unique($tests);
+    }
+
+    public function test(string $test)
+    {
+        $callable = [$this, "test_" . $test];
+        printf("Running Test [%s]...\n", $test);
+        [$result, $addColumns] = is_callable($callable) ? call_user_func($callable) : $this->test_default($test);
+        printf("Tests closed.\n");
+        $size = count($result);
+        printf("%d Results.\n", $size);
+        if ($size > 0) {
+            printf("%d Languages.\n", count(array_keys(current($result))) - $addColumns);
+        }
+
+        return $result;
 
     }
-    $results[] = $result;
+
+    private function test_bubble()
+    {
+        $firstElementSize = $this->confs["bubble"]["firstElementSize"] ?? 500;
+        $lastElementSize = $this->confs["bubble"]["lastElementSize"] ?? 5000;
+        $step = $this->confs["bubble"]["step"] ?? 500;
+
+        $results = [];
+        for ($count = $firstElementSize; $count <= $lastElementSize; $count = $count + $step) {
+            $results[] = array_merge(["elements" => $count], $this->test_default("bubble", [$count])[0]);
+        }
+        return [$results, 1];
+    }
+
+    private function exec(string $exec, string $file, array $args = [], callable $logger = null)
+    {
+        $args = implode(" ", $args);
+        $command = str_replace(["$0", "$1"], [$file, $args], $exec);
+        $result = 0;
+        $repeats = $this->repeats > 0 ? $this->repeats : 1;
+
+        for ($s = 0; $s < $repeats; $s++) {
+            $return = exec($command . ' ' . $args);
+            if (is_callable($logger)) $logger($args . "\t|\t" . $return);
+            preg_match('/^(.*?)\[micros\]$/', $return, $match);
+            //Durchschnitt berechnen
+            $result = ($result * $s + intval($match[1])) / ($s + 1);
+        }
+        $result = intval($result);
+        if ($repeats > 1) {
+            if (is_callable($logger)) $logger("AVG:\t|\t" . $result . "[micros]");
+        }
+        return $result;
+    }
+
+    private function test_default(string $test, array $args = [])
+    {
+        $result = [];
+        foreach ($this->languages as $lang => $langConf) {
+            if (is_file($langConf["files"][$test] ?? "")) {
+                $result[$lang] = $this->exec(
+                    $langConf["exec"],
+                    $langConf["files"][$test],
+                    $args,
+                    function ($msg) use ($lang) {
+                        echo $lang . "\t" . $msg . PHP_EOL;
+                    });
+            }
+        }
+        return [$result, 0];
+    }
 }
 
-$csvHead = array_keys(current($results));
-$csv = implode(";", $csvHead) . "\n";
-foreach ($results as $result) {
-    $csv .= implode(";", $result) . "\n";
+function saveResultAsCSV(array $results, string $file)
+{
+    if (empty($results)) return;
+    $csvHead = array_keys(current($results));
+    $csv = implode(";", $csvHead) . "\n";
+    foreach ($results as $result) {
+        $csv .= implode(";", $result) . "\n";
+    }
+    file_put_contents($file, $csv);
 }
-file_put_contents($outputfile, $csv);
---
+++ perf/java/bubble.java
@@ -0,0 +1,30 @@
+class Main{
+
+	static void test(int[] array, int length){
+		int tmp;
+		for (int n= length; n>1; --n){
+			for (int i=0; i<n-1; ++i){
+				if (array[i] > array[i+1]){
+					tmp = array[i];
+					array[i] = array[i+1];
+					array[i+1]=tmp;
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args){
+			long start, end;
+			int size = Integer.parseInt(args[0]);
+			int[] array = new int[size];
+			for(int i = 0; i<size; i++){
+					array[i]=size-i;
+			}
+
+			start = System.nanoTime();
+			test(array, size);
+			end = System.nanoTime();
+
+			System.out.println( (end-start)/1000+"[micros]");
+	}
+}
